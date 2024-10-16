2a18332c74dca9051327ce3ab89327e233933054
ABINASH A. <allesabinash1@gmail.com>
2024-10-16 17:26:35 +0530

    add user profile api

--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -1,11 +1,30 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import MyUserController from '../controllers/MyUserController';
-import { jwtCheck } from '../middleware/auth';
+import { jwtCheck, jwtParse } from '../middleware/auth';
+import { validateMyUserRequest } from '../middleware/validation';
 
 const router = express.Router();
 
 router.post("/",jwtCheck, (req, res) => {
     MyUserController.createCurrentUser(req, res);
 });
+
+// router.put("/",
+//      jwtCheck,
+//      (req:Request,res:Response,next:NextFunction)=>{jwtParse(req,res,next)},
+//      validateMyUserRequest, 
+//      (req, res) => {
+//     MyUserController.updateCurrentUser(req, res);
+// });
+
+router.put(
+    "/",
+    jwtCheck,
+    (req: Request, res: Response, next: NextFunction) => { jwtParse(req, res, next); },
+    ...validateMyUserRequest as express.RequestHandler[],  // Explicit type assertion
+    (req: Request, res: Response) => {
+        MyUserController.updateCurrentUser(req, res);
+    }
+);
 
 export default router;
