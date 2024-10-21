663333eabb380d6485bb5f5e12b95cccf30ca775
ABINASH A. <allesabinash1@gmail.com>
2024-10-21 06:37:13 +0530

    search api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,8 @@ import mongoose from 'mongoose';
 import myUserRoute from './routes/MyUserRoute';
 import { v2 as cloudinary } from "cloudinary";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
+import restaurantRoute from "./routes/RestaurantRoute";
+
 
 
 
@@ -28,6 +30,8 @@ app.get("/health", async (req: Request, res: Response) => {
 
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
+app.use("/api/restaurant", restaurantRoute);
+
 
 app.listen(7000,()=>{
     console.log("Server started at localhost:7000");
