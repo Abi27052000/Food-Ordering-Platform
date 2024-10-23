e70b0a0d9aae9c34282ce5de4dea3927cc54c3ce
ABINASH A. <allesabinash1@gmail.com>
2024-10-23 06:17:55 +0530

    search page layout

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,22 +1,58 @@
 import { useSearchRestaurants } from "@/api/RestaurantApi";
+import SearchResultCard from "@/components/SearchResultCard";
+import SearchResultInfo from "@/components/SearchResultInfo";
 import { useParams } from "react-router-dom"
 
 const SearchPage = () => {
  const {city} = useParams();
- const {results} = useSearchRestaurants(city);
+ const {results,isLoading} = useSearchRestaurants(city);
+
+ if(isLoading){
+     return <span>Loading.....</span>
+ }
+
+ if(!results?.data|| !city){
+    return <span>No results found</span>
+ }
 
  return (
-    <span>
-        User searched for {city} {" "}
-        <span>
-            {results?.data.map((restaurant) => (
-                <span >
-                    found-{restaurant.restaurantName}, {restaurant.city}
-                </span>
-                ))}
-        </span>
-    </span>
- )
+    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+      <div id="cuisines-list">insert cuisines here
+        {/* <CuisineFilter
+          selectedCuisines={searchState.selectedCuisines}
+          onChange={setSelectedCuisines}
+          isExpanded={isExpanded}
+          onExpandedClick={() =>
+            setIsExpanded((prevIsExpanded) => !prevIsExpanded)
+          }
+        /> */}
+      </div>
+      <div id="main-content" className="flex flex-col gap-5">
+        {/* <SearchBar
+          searchQuery={searchState.searchQuery}
+          onSubmit={setSearchQuery}
+          placeHolder="Search by Cuisine or Restaurant Name"
+          onReset={resetSearch}
+        /> */}
+        <div className="flex justify-between flex-col gap-3 lg:flex-row">
+          <SearchResultInfo total={results.pagination.total} city={city} />
+          {/* <SortOptionDropdown
+            sortOption={searchState.sortOption}
+            onChange={(value) => setSortOption(value)}
+          /> */}
+        </div>
+
+        {results.data.map((restaurant) => (
+          <SearchResultCard restaurant={restaurant} />
+        ))}
+        {/* <PaginationSelector
+          page={results.pagination.page}
+          pages={results.pagination.pages}
+          onPageChange={setPage}
+        /> */}
+      </div>
+    </div>
+  );
 }
 
 export default SearchPage
